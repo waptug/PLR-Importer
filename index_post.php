c5b625656729256e3de5fc1b80d2234b69c331ea
Michael McGinn <geekzonehostingllc@gmail.com>
2022-04-03 16:18:54 -0700

    copied code from live server

--- index_post.php
+++ index_post.php
@@ -6,6 +6,7 @@
 * Co-developed by: Michael Scott McGinn and Ed Reel
 * Copyright 2022 by GeekZoneHosting.Com, LLC
 ********************************************************/
+//The section extracts the files from the .zip and creates a folder to store the .txt files
     if (isset($_POST["btn_zip"])) {
         $output = '';
         if ($_FILES['zip_file']['name'] != '') {
@@ -31,20 +32,27 @@
             }
         }
     }
+    
+    //Code added to display progress. When we get the code working we can comment this out.
+    $domain="https://plrimporter.com/";//hard coded but would need to change when it gets installed on another domain.
+    
     echo "files upconpressed";
-    echo "<br/>The Path is: ".$path;
+    echo "<br/>The Path is to your file is: ".$location."<br/>";
+    
+    // End of progress display
+    
     foreach(glob($path.'/*.*') as $file) {
-         echo $file."<br>";
+         echo "<br/>".$file."<br>";
         $single_txt_file = fopen($file, "r");
         $first_line = true;
         while (!feof($single_txt_file)) {
             $line = fgets($single_txt_file);
 
              if($first_line){
-                 echo "<h1>$line</h1>";
+                 echo "<h1>$line</h1>";// This is the title
                  $first_line = false;
              }else{
-                 echo $line . "<br>";
+                 echo $line . "<br>";//This is the rest of the body of the article.
              }
             $data[] = $line;
         }
@@ -60,38 +68,37 @@
             }
         }
     
+    echo "========= array dump ========<br/>";
         print_r($new_data[0]);
+    echo "==============================<br/>";    
         
         $main_data = [
-            ['Title', 'Details'],
+            ['Title', 'body'],
             ['My Title', $new_data[0]]
         ];
 
         $filename = $auto_generated.'.csv';
-
+echo "=============<br/>";
+echo "name of result file:".$filename."<br/>";
+echo "=============<br/>";
         // open csv file for writing
         $f = fopen($filename, 'w');
 
         if ($f === false) {
             die('Error opening the file ' . $filename);
         }
-        /*
-        Some how this code needs to catch each file as it passes through the loop to split it out
 
-        $title = strtok($content, "\n");
-        $content = trim(substr($content, strpos($content, "\n") + 1));
-
-        The we will have the title in $title and the body content in $content. That we can meld into the csv flow.
-        
-        */
         // write each row at a time to a file
+        $counter=0;
         foreach ($main_data as $row) {
             fputcsv($f,
                 $row
             );
+            $counter+=1;
+            echo "Row:".$counter.":".$row."<br/>";
         }
 
         // close the file
         fclose($f);
     }
-?>  
+?>
