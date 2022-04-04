98714cafcb109115449968bd18d393fc159f5cb5
Michael McGinn <geekzonehostingllc@gmail.com>
2022-04-04 14:31:13 -0700

    Updated front page index to match production server

--- index.php
+++ index.php
@@ -1,29 +1,51 @@
-<p style="text-align: center;">PLR Importer</p>
-<!--?php 
+<p style="text-align: center;"><img title="PLRImporter.Com Logo" src="PLR-Importer-Logo.png" alt="PLRImporter.Com Logo" width="549" height="258" /></p>
+<p style="text-align: center;">&nbsp;</p>
+<p><span style="font-size: xx-large;"><!--?php 
 
 // echo "PLR Importer";
-?-->
-<h1 style="text-align: center;">PLR Importer</h1>
-<p style="text-align: center;">https://plrimporter.com</p>
-<h2 style="text-align: center;">Private Lable Rights Article Importer</h2>
+?--></span></p>
+<h1 style="text-align: center;"><span style="font-size: xx-large;">PLR Importer</span></h1>
+<p style="text-align: center;"><span style="font-size: xx-large;">https://plrimporter.com</span></p>
+<h2 style="text-align: center;"><span style="font-size: xx-large;">Private Lable Rights Article Importer</span></h2>
 <hr style="height: 2px; width: 80%;" size="2" width="80%" />
 <p>&nbsp;</p>
-<p style="text-align: center;"><span style="font-size: large; font-family: trebuchet ms, geneva;">PLR Importer is a SaaS application to allow you to convert PLR article packs into .csv files ready to be imported into your Drupal or WordPress Blog as Article Nodes or Blog Posts.</span></p>
-<p style="text-align: center;"><span style="font-size: large; font-family: trebuchet ms, geneva;">We are in the Alpha Stage of Development at this point and we are looking for angle funding for our project.</span></p>
-<p style="text-align: center;"><span style="font-size: large; font-family: trebuchet ms, geneva;">Search for "PLR Importer" on your favorite search engine and see our success at placing on the front page of 3 of the 4 major search engines (Yahoo.com, Bing.Com and DuckDuckGo.Com) in only 8 hours after posting.</span></p>
-<p style="text-align: center;"><span style="font-size: large; font-family: trebuchet ms, geneva;">Google.Com evidently has a slower spider and has not indexed us yet. Check and report back to us if you see us on the first page for our search term "PLR Importer"</span></p>
+<p style="text-align: center;"><span style="font-size: large; font-family: trebuchet ms, geneva;">PLR Importer is a SaaS application to allow you to upload and convert PLR article .zip packs into .csv files ready to be imported </span></p>
+<p style="text-align: center;"><span style="font-size: large; font-family: trebuchet ms, geneva;">into your Drupal or WordPress Blog as Article Nodes or Blog Posts.</span></p>
+<p style="text-align: center;">&nbsp;</p>
+<p style="text-align: center;"><span style="font-family: trebuchet ms, geneva; color: #008000; font-size: x-large;">We are in the Alpha Stage of Development at this point and we are looking for <a title="PLR Importer Angle Funding Site " href="https://www.givesendgo.com/plrimporter" target="_blank">angle funding</a> for our project.</span></p>
+<p style="text-align: center;"><span style="font-size: large; font-family: trebuchet ms, geneva;">Search for "PLR Importer" on your favorite search engine and see our success at placing on the </span></p>
+<p style="text-align: center;"><span style="font-size: large; font-family: trebuchet ms, geneva;">front page of 3 of the 4 major search engines (Yahoo.com, Bing.Com and DuckDuckGo.Com) in only 8 hours after posting.</span></p>
+<p style="text-align: center;"><span style="font-size: large; font-family: trebuchet ms, geneva;">Google.Com evidently has a slower spider and it took 2 days, see below for screen shots. </span></p>
+<p style="text-align: center;"><span style="font-size: large; font-family: trebuchet ms, geneva;">&nbsp;</span></p>
 <p style="text-align: center;"><span style="font-size: large; font-family: trebuchet ms, geneva;">Visit our git hub at <a href="https://github.com/waptug/PLR-Importer" target="_blank">https://github.com/waptug/PLR-Importer</a> to get technical details, funding links and join our development team.</span></p>
 <p style="text-align: center;"><span style="font-size: large; font-family: trebuchet ms, geneva;"><a href="mailto:launchparty@plrimporter.com">Email us for an invitation to the launch party at&nbsp; launchparty@plrimporter.com</a></span></p>
 <p style="text-align: center;">&nbsp;</p>
+<p style="text-align: center;">&nbsp;</p>
+<hr style="height: 2px; width: 80%;" size="2" width="80%" />
+<p style="text-align: center;">&nbsp;</p>
+<p style="text-align: center;">&nbsp;</p>
 <p style="text-align: center;"><span style="font-size: large; font-family: trebuchet ms, geneva;">Get your blog ready to load with 1000's of PLR articles with a single Up Load.</span></p>
-<p style="text-align: center;"><span style="font-size: large; font-family: trebuchet ms, geneva;">Need Hosting see our Project Sponsor at <a title="MTBN.NET Hosting" href="https://mtbn.net" target="_blank">https://mtbn.net</a></span></p>
+<p style="text-align: center;"><span style="font-size: small; font-family: trebuchet ms, geneva;">Need Hosting see our Project Sponsor at <a title="MTBN.NET Hosting" href="https://mtbn.net" target="_blank">https://mtbn.net</a></span></p>
+<p style="text-align: center;"><span style="font-size: small; font-family: trebuchet ms, geneva;">Special offer - Get 10,000 PLR articles to use for free with your Paid Hosting Account</span></p>
+<p style="text-align: center;"><span style="font-size: small; font-family: trebuchet ms, geneva;">To Redeam this offer just email us with the subject "I WANT THE PLR OFFER" and attach your Domain Certificate PDF you get when you buy it at MTBN.NET to <a href="mailto:plroffer@plrimporter.com?subject=&quot;I WANT THE PLR OFFER&quot;&amp;body=&quot;Instructions: Attache the Domain Certificate PDF you receive when you register a domain at MTBN.NET and we will send you a sample pack of 10,000 PLR articles you can use.&quot;">plroffer@plrimporter.com</a></span></p>
+<p style="text-align: center;">&nbsp;</p>
+<hr style="height: 2px; width: 80%;" size="2" width="80%" /><hr style="height: 2px; width: 80px;" size="2" width="80" />
 <p style="text-align: center;">&nbsp;</p>
 <p style="text-align: center;"><span style="font-size: large; font-family: trebuchet ms, geneva;">Screen Shots of our Search Engine Placements</span></p>
 <p style="text-align: center;">&nbsp;</p>
-<p style="text-align: center;"><strong><span style="font-size: x-large; font-family: trebuchet ms, geneva;">DuckDuckGo.Com</span></strong></p>
+<p style="text-align: center;"><strong><span style="font-size: x-large; font-family: trebuchet ms, geneva;">DuckDuckGo.Com - 4/2/2022<br /></span></strong></p>
 <p style="text-align: center;"><span style="font-size: large; font-family: trebuchet ms, geneva;"><img title="DuckDuckGo Search Results for &quot;PLR Importer&quot;" src="DuckDuckGo.png" alt="DuckDuckGo Search Results" /></span></p>
-<p style="text-align: center;"><strong><span style="font-size: x-large;">&nbsp;BING.COM</span></strong></p>
+<p style="text-align: center;"><strong><span style="font-size: x-large;">&nbsp;BING.COM -4/2/2022<br /></span></strong></p>
 <p style="text-align: center;"><img title="Bing.Com Search Engin Results" src="Bing.png" alt="Bing.Com search Engine Results" width="1920" height="1080" /></p>
 <p>&nbsp;</p>
-<p style="text-align: center;"><strong><span style="font-size: x-large;">YAHOO.COM</span></strong></p>
+<p style="text-align: center;"><strong><span style="font-size: x-large;">YAHOO.COM - 4/2/2022<br /></span></strong></p>
 <p style="text-align: center;"><strong><span style="font-size: x-large;"><img title="Yahoo.Com Search Results" src="Yahoo.png" alt="Yahoo.Com Search Results" width="1796" height="2221" /></span></strong></p>
+<p style="text-align: center;"><strong><span style="font-size: x-large;">We Made it to the Front Page of GOOGLE.COM - 4/3/2022</span></strong></p>
+<p style="text-align: center;"><img title="Google Search Engin Results for &quot;PLR Importer&quot;" src="Google.png" alt="Google Search Engin Results for &quot;PLR Importer&quot;" width="1796" height="3101" /></p>
+<p style="text-align: center;">&nbsp;</p>
+<hr style="height: 2px; width: 80%;" size="2" width="80%" />
+<p style="text-align: center;">Do you market a product related to the Private Lable Rights industry that does not directly compete with us and our offer?</p>
+<p style="text-align: center;">Pitch it to us as a Joint Venture offer and you could see it linked here.</p>
+<p style="text-align: center;">Send us an email at <a href="mailto:jvpitches@plrimporter.com?subject=JV Pitch Offer for PLR Importer SIte&amp;body=We saw your site plrimporter.com and wanted to share our JV Pitch with you.">JVPitchs@plrimporter.com</a></p>
+<p style="text-align: center;">&nbsp;</p>
+<p style="text-align: center;">&nbsp;</p>
