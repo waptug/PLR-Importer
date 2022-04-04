2255eaa4c202a6e42a22522b03aba17ccddc8430
Michael McGinn <geekzonehostingllc@gmail.com>
2022-04-04 14:21:17 -0700

    added new code

--- index_post.php
+++ index_post.php
@@ -7,6 +7,8 @@
 * Copyright 2022 by GeekZoneHosting.Com, LLC
 ********************************************************/
 //The section extracts the files from the .zip and creates a folder to store the .txt files
+echo "Uncompressing files.<br/>";
+$filecounter=0;
     if (isset($_POST["btn_zip"])) {
         $output = '';
         if ($_FILES['zip_file']['name'] != '') {
@@ -31,31 +33,55 @@
                 }
             }
         }
+        $filecounter=+1;
     }
     
     //Code added to display progress. When we get the code working we can comment this out.
     $domain="https://plrimporter.com/";//hard coded but would need to change when it gets installed on another domain.
     
-    echo "files upconpressed";
-    echo "<br/>The Path is to your file is: ".$location."<br/>";
+    echo $filecounter." files upconpressed<br/>";
+    echo "==================<br/>The Path to your files is: ".$location."<br/>=============<br/>";
     
     // End of progress display
     
+    //Grabs the list of file paths for each file in the directory and loops through them putting them into $file
+    //Everything happens in this loop to parse each file line by line in to $data array and create the .csv file
+    $line_counter=0;//display the array position line is put in todo:fix it so it works for entire loop. 
+
     foreach(glob($path.'/*.*') as $file) {
-         echo "<br/>".$file."<br>";
+         echo "<br/>====== processing file name ==========<br/>".$file."<br>========================<br/>";
         $single_txt_file = fopen($file, "r");
         $first_line = true;
+        
         while (!feof($single_txt_file)) {
-            $line = fgets($single_txt_file);
+            $line = fgets($single_txt_file);// grabs one line of the file that the pointer is pointing to as a string $line
+         
+            //Test code to split up the file, not part of the orginal file
 
+            //$title = strtok($single_txt_file, "\n");
+            //$content = trim(substr($single_txt_file, strpos($single_txt_file, "\n") + 1));
+            
+
+            //echo "line var=".$line."<br/>Title=".$title."<br/>Content=".$content."<br/>";
+            // end of test code
+
+             
              if($first_line){
-                 echo "<h1>$line</h1>";// This is the title
-                 $first_line = false;
-             }else{
-                 echo $line . "<br>";//This is the rest of the body of the article.
-             }
-            $data[] = $line;
+                echo "<br/>============ data[".$line_counter."] =============================</br>";
+                echo "<h1>$line</h1>";// if this is the first line of the file then this is the title
+                echo "===============================================================<br/>";
+                $first_line = false;
+                }else{
+                    //$line_counter++;
+                 echo "<br/>======== data[".$line_counter."] ================<br/>";   
+                 echo $line . "<br>===============================<br/>";//This is the rest of the body of the article one line at a time.
+                }
+            $data[] = $line;//puts line into the array $data[]
+            //if($first_line$){data[]+=",";}
+            $line_counter++;
         }
+        $arraylength=count($data);
+        echo "<br/><h1>data contains:".$arraylength."-data[3]=".$data[3]."=print_r=".print_r($data)."<br/></h1>";
 
         $new_data[0] = "";
         foreach($data as $da){
@@ -68,9 +94,9 @@
             }
         }
     
-    echo "========= array dump ========<br/>";
+    echo "<br/>========= array dump new_data[0] array ========<br/>";
         print_r($new_data[0]);
-    echo "==============================<br/>";    
+    echo "<br/>==============================<br/>";    
         
         $main_data = [
             ['Title', 'body'],
@@ -78,11 +104,11 @@
         ];
 
         $filename = $auto_generated.'.csv';
-echo "=============<br/>";
-echo "name of result file:".$filename."<br/>";
-echo "=============<br/>";
-        // open csv file for writing
-        $f = fopen($filename, 'w');
+echo "<br/>==================================================<br/>";
+echo "name of file csv will be written into:".$filename;
+echo "<br/>===================================================<br/>";
+        
+        $f = fopen($filename, 'w');// open csv file for writing
 
         if ($f === false) {
             die('Error opening the file ' . $filename);
@@ -91,14 +117,20 @@ echo "=============<br/>";
         // write each row at a time to a file
         $counter=0;
         foreach ($main_data as $row) {
-            fputcsv($f,
-                $row
-            );
+            fputcsv($f,$row,",");
             $counter+=1;
-            echo "Row:".$counter.":".$row."<br/>";
+            echo "<br/>Row:".$counter.":<br/>=======================<br/>".print_r([$row])."<br/>=================<br>";
         }
 
         // close the file
         fclose($f);
     }
+    echo "<br/>================ Processing Completed =====================<br/>";
+    echo "Your result is here:".$filename;
+
 ?>
+<html>
+    <body>
+        <a href="<?php echo $filename; ?>"><?php echo $filename; ?></a>
+</body>
+</html>
